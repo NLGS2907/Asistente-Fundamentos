ba74a3f5c3b7704a0dea9cb9e466333b955ad610
Franco Lighterman Reismann <flighterman@fi.uba.ar>
2025-07-28 20:59:23 -0300

    refactor: info redundante en `main`
    Esto irá en clases especializadas.

--- asistente/main.py
+++ asistente/main.py
@@ -1,15 +1,6 @@
 #pylint: disable=line-too-long
 """
-Módulo Principal
-
-Permisos necesarios: 'Send Messages' - 'Manage Messages' - "Read Message History" -
-                     'Send Messages in Threads' - 'Manage Threads' - 'Mention Everyone' -
-                     'Read Messages/View Channels' - 'Add Reactions' - 'Use Slash Commands'
-
-Permissions Integer: 19327560768
-
-Enlace para invitar bot: https://discord.com/api/oauth2/authorize?client_id=889312376036425810&permissions=294205467712&scope=bot%20applications.commands
-Repositorio: https://github.com/NLGS2907/Asistente-Fundamentos
+Módulo Principal.
 """
 
 from os import getenv
