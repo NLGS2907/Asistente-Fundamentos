541aae8121eb70fc38c286465274e9bbab838a8c
Franco Lighterman Reismann <flighterman@fi.uba.ar>
2025-07-29 23:12:46 -0300

    refactor: cargar y guardar JSON ya soporta file-likes
    Como buffers de IO.

--- asistente/archivos/json.py
+++ asistente/archivos/json.py
@@ -3,33 +3,40 @@ Módulo que procesa archivos en format oJSON.
 """
 
 from json import dump, load
-from typing import TypeAlias, TYPE_CHECKING
+from os import PathLike
+from typing import TextIO, TypeAlias, Union
 
-if TYPE_CHECKING:
-    from os import PathLike
-
+RutaOArchivo: TypeAlias = Union[PathLike, TextIO]
 DiccionarioPares: TypeAlias = dict[str, str]
 
 
-def cargar_json(ruta_archivo: "PathLike") -> DiccionarioPares:
+def cargar_json(archivo: RutaOArchivo) -> DiccionarioPares:
     """
     Lee y carga un archivo JSON.
     """
 
     dic_pares_valores = {}
 
-    with open(ruta_archivo, mode='r', encoding="utf-8") as archivo:
+    if isinstance(archivo, (str, bytes, PathLike)):
+        with open(archivo, mode='r', encoding="utf-8") as arch:
+            dic_pares_valores = load(arch)
+
+    else:
         dic_pares_valores = load(archivo)
 
     return dic_pares_valores
 
 def guardar_json(dic_pares_valores: DiccionarioPares,
-                 ruta_archivo: "PathLike",
+                 archivo: RutaOArchivo,
                  *,
                  sangria: int=4) -> None:
     """
     Recibe un diccionario y guarda la informacion del mismo en un archivo JSON.
     """
 
-    with open(ruta_archivo, mode='w', encoding="utf-8") as archivo:
+    if isinstance(archivo, (str, bytes, PathLike)):
+        with open(archivo, mode='w', encoding="utf-8") as arch:
+            dump(dic_pares_valores, arch, indent=sangria)
+
+    else:
         dump(dic_pares_valores, archivo, indent=sangria)
